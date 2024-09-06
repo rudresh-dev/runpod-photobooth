5093f78f4555569bf9e2e956a84d2b56efce4858
rudresh-dev <rudresh@craftech360.com>
2024-09-06 20:51:15 +0530

    sdfdfdfdfgfdfgf

--- src/pages/PresetPage.jsx
+++ src/pages/PresetPage.jsx
@@ -127,7 +127,7 @@ import { savePresetImage } from '../api';
 const PresetPage = () => {
   const [selectedPresetImage, setSelectedPresetImage] = useState(null);
   const [isSaving, setIsSaving] = useState(false);
-  const [gender, setGender] = useState(2); // Default to male (2)
+  const [gender, setGender] = useState(1); // Default to male (2)
   const navigate = useNavigate();
 
   const presetImages = [
