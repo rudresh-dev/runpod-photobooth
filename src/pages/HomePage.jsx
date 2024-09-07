faf37d85a4458cb89254650a9d9f9fcf084028dd
rudresh-dev <rudresh@craftech360.com>
2024-09-07 16:23:40 +0530

    kdjd

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   return (
     <div className=" flex-col md:flex-colw-screen h-screen flex justify-center items-center gap-5">
       <button className='bg-none border-2 border-white px-16 py-4 rounded-2xl text-white text-3xl ' onClick={() => navigate('/prompt')}>Prompt</button>
-      <button className='bg-none border-2 border-white px-16 py-4 rounded-2xl text-white text-3xl' onClick={() => navigate('/preset')}>Preset</button>
+      {/* <button className='bg-none border-2 border-white px-16 py-4 rounded-2xl text-white text-3xl' onClick={() => navigate('/preset')}>Preset</button> */}
     </div>
   );
 };
